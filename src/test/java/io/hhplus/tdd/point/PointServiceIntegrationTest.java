d089b50de16a700322ea5a3c490fb036f012e836
jhshin29 <shinji03624@gmail.com>
2024-09-25 23:35:17 +0900

    feat: for integration test

--- src/test/java/io/hhplus/tdd/point/PointServiceIntegrationTest.java
+++ src/test/java/io/hhplus/tdd/point/PointServiceIntegrationTest.java
@@ -1,5 +1,7 @@
 package io.hhplus.tdd.point;
 
+import io.hhplus.tdd.point.repository.PointHistoryRepository;
+import io.hhplus.tdd.point.repository.UserPointRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,4 +10,12 @@ public class PointServiceIntegrationTest {
 
     @Autowired
     private PointService pointService;
+
+    @Autowired
+    private UserPointRepository userPointRepository;
+
+    @Autowired
+    private PointHistoryRepository pointHistoryRepository;
+
+
 }
